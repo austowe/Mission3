5f74b6dafc7cf105c3ddfd915f7c8ca5ecf10106
CameronPike <cp475@byu.edu>
2023-01-23 17:14:19 -0700

    comments

--- Mission3/Program.cs
+++ Mission3/Program.cs
@@ -8,31 +8,39 @@ namespace Mission3
         static void Main(string[] args)
         {
             Ticker TKR = new Ticker();
+            // Welcome Message
             Console.WriteLine("Welcome to the game dude!");
             int[] game_board = new int[9];
             TKR.printBoard(game_board);
 
+            // Loop through each turn
             for (int i = 0; i < 10; i++)
             {
+                // Check game progress, end it if there is a winner or draw
                 int winCheck = TKR.winCheck(game_board);
                 if (winCheck == 4)
                 {
+                    // Differentiate players based on assignment of 1 and 2
                     if (i % 2 == 0)
                     {
+                        // Players enter their choice
                         Console.WriteLine("X's Choice: ");
                         int answer = Convert.ToInt32(Console.ReadLine());
+                        // Check if the number as been selected already
                         bool exists = Array.Exists(game_board, element => element == answer);
                         while (exists)
                         {
-                            Console.WriteLine("Already Chosen. X's Choice: ");
+                            Console.WriteLine("The Selection " + answer + " Has Already Been Chosen. X Choose Again: ");
                             int new_answer = Convert.ToInt32(Console.ReadLine());
                             exists = Array.Exists(game_board, element => element == new_answer);
                             if (!exists)
                             {
+                                // enter player entry into array game_board and print the state of the board
                                 game_board[new_answer - 1] = (2);
                                 TKR.printBoard(game_board);
                             }
                         }
+                        // enter player entry into array game_board and print the state of the board
                         game_board[answer - 1] = (2);
                         TKR.printBoard(game_board);
                     }
@@ -43,19 +51,24 @@ namespace Mission3
                         bool exists = Array.Exists(game_board, element => element == answer);
                         while (exists)
                         {
-                            Console.WriteLine("Already Chosen. O's Choice: ");
+                            // Players enter their choice
+                            Console.WriteLine("The Selection " + answer + " Has Already Been Chosen. O Choose Again: ");
                             int new_answer = Convert.ToInt32(Console.ReadLine());
+                            // Check if the number as been selected already
                             exists = Array.Exists(game_board, element => element == new_answer);
                             if (!exists)
                             {
+                                // enter player entry into array game_board and print the state of the board
                                 game_board[new_answer - 1] = (1);
                                 TKR.printBoard(game_board);
                             }
                         }
+                        // enter player entry into array game_board and print the state of the board
                         game_board[answer - 1] = (1);
                         TKR.printBoard(game_board);
                     }
                 }
+                // game status checks
                 else if (winCheck == 1)
                 {
                     Console.WriteLine("X's Wins!");
